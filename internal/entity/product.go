825532d3eea75eef245cc4827a553d686a3b8bef
vitorcontiz <vitorcontizgit@gmail.com>
2022-12-25 22:01:33 -0300

    feat: product entity

---
+++ internal/entity/product.go
@@ -0,0 +1,56 @@
+package entity
+
+import (
+	"errors"
+	"time"
+
+	"github.com/vitorconti/go-user-products-api/pkg/entity"
+)
+
+var (
+	ErrIdIsRequired    = errors.New("Id is required")
+	ErrInvalidId       = errors.New("Invalid Id")
+	ErrNameIsRequired  = errors.New("Name is required")
+	ErrPriceIsRequired = errors.New("Price is required")
+	ErrInvalidPrice    = errors.New("Invalid price")
+)
+
+type Product struct {
+	ID        entity.ID `json:"id"`
+	Name      string    `json:"name"`
+	Price     int       `json:"price"`
+	CreatedAt time.Time `json:"CreatedAt"`
+}
+
+func NewProduct(name string, price int) (*Product, error) {
+	product := &Product{
+		ID:        entity.NewID(),
+		Name:      name,
+		Price:     price,
+		CreatedAt: time.Now(),
+	}
+	err := product.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+func (p *Product) Validate() error {
+	if p.ID.String() == "" {
+		return ErrIdIsRequired
+	}
+	if _, err := entity.ParseID(p.ID.String()); err != nil {
+		return ErrInvalidId
+	}
+	if p.Name == "" {
+		return ErrNameIsRequired
+	}
+	if p.Price == 0 {
+		return ErrPriceIsRequired
+	}
+	if p.Price <= 0 {
+		return ErrInvalidPrice
+	}
+	return nil
+}

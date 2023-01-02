0aa6c0842e1036bfbf1e3693271a34f48d39f4e8
vitorcontiz <vitorcontizgit@gmail.com>
2023-01-01 21:46:18 -0300

    feat: create product method

---
+++ internal/infra/database/product_db.go
@@ -0,0 +1,18 @@
+package database
+
+import (
+	"github.com/vitorconti/go-user-products-api/internal/entity"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	DB *gorm.DB
+}
+
+func NewProduct(db *gorm.DB) *Product {
+	return &Product{DB: db}
+}
+
+func (p *Product) Create(product *entity.Product) error {
+	return p.DB.Create(product).Error
+}

51fd4320fda8d99bd9e2f7031dfafda28149f3c9
vitorcontiz <vitorcontizgit@gmail.com>
2023-01-01 21:54:29 -0300

    feat: delete method for products

--- internal/infra/database/product_db.go
+++ internal/infra/database/product_db.go
@@ -30,3 +30,11 @@ func (p *Product) Update(product *entity.Product) error {
 	}
 	return p.DB.Save(product).Error
 }
+
+func (p *Product) Delete(id string) error {
+	_, err := p.FindByID(id)
+	if err != nil {
+		return err
+	}
+	return p.DB.Delete(id).Error
+}
